d1f591492b381db091b47e03d16c37c035de0268
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-02-05 01:40:39 -0300

    feat: page register

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,11 @@
+import FormRegister from "../../components/FormRegister/index";
+
+const PageRegister = () => {
+  return (
+    <div>
+      <FormRegister />
+    </div>
+  );
+};
+
+export default PageRegister;

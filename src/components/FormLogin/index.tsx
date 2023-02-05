b2bba699dbee443488019f12d13751df204df2fd
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-02-05 20:34:02 -0300

    feat: create form login

---
+++ src/components/FormLogin/index.tsx
@@ -0,0 +1,43 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import formLoginSchema from "./schema";
+import { IInputKeys } from "../Input/interface";
+import Input from "../Input";
+import useClientContext from "../../context/clientContext";
+
+const FormLogin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IInputKeys>({
+    resolver: yupResolver(formLoginSchema),
+  });
+
+  const { login } = useClientContext();
+
+  return (
+    <form onSubmit={handleSubmit(login)}>
+      <Input
+        id="email"
+        label="Email"
+        type="text"
+        required
+        register={register}
+        error={errors.email?.message}
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="text"
+        required
+        register={register}
+        error={errors.password?.message}
+      />
+
+      <input type="submit" value="Logar" />
+    </form>
+  );
+};
+
+export default FormLogin;
